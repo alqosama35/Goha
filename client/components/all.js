ee8dec293d17359bb40d6cb42e74911657138bfa
alqosama35 <alqosama35@gmail.com>
2024-11-19 18:34:26 +0200

    how to make new edit

---
+++ client/components/all.js
@@ -0,0 +1,10 @@
+//--make new branch
+// git branch <branch-name>
+//-- add your edits
+// git add .
+//--commit your edits
+// git commit -m "message write a describtion for your edits"
+//--push you edits to the branch you made
+// git push origin <branch-name>
+//--merge your edits with the main on your new branch
+// git merge main
