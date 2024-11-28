7f8a7e3213502246ccb5f30754bc74d7e43ace0f
alqosama35 <alqosama35@gmail.com>
2024-11-28 13:07:39 +0200

    add all.js how to push

--- client/components/all.js
+++ client/components/all.js
@@ -10,3 +10,20 @@
 // git push origin <branch-name>
 //--merge your edits with the main on your new branch
 // git merge main
+//------>to merge your new branch on the main:
+// 1- go to branch main
+// git checkout main
+// 2- merge 
+// git merge newBranchName
+
+//---------------------------------------------------------
+
+// if you want to push your edits directly on main:
+// 1- select the branch main
+// git checkout main
+// 2- do you edits then add and commit
+// git add .
+// git commit -m "your comment here"
+// 3- push directly to main
+// push origin main
+// Done, Good Job :)
